65e30d68e25dc62e346d153142eb99e91dcc6e70
Rumaan <rumaankalander@gmail.com>
2018-05-04 19:20:37 +0530

    Added retrofit and moshi converters

--- app/src/main/java/me/haxzie/qrreader/MainActivity.java
+++ app/src/main/java/me/haxzie/qrreader/MainActivity.java
@@ -1,127 +1,144 @@
 package me.haxzie.qrreader;
 
-import android.app.ProgressDialog;
+import android.content.DialogInterface;
+import android.content.DialogInterface.OnDismissListener;
 import android.graphics.PointF;
-import android.support.design.widget.FloatingActionButton;
-import android.support.v7.app.AlertDialog;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.ImageButton;
-import android.widget.TextView;
 import android.widget.Toast;
-
 import com.dlazaro66.qrcodereaderview.QRCodeReaderView;
+import com.dlazaro66.qrcodereaderview.QRCodeReaderView.OnQRCodeReadListener;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
-import java.io.IOException;
+public class MainActivity extends AppCompatActivity implements
+    OnQRCodeReadListener, OnDismissListener, View.OnClickListener {
 
-import okhttp3.FormBody;
-import okhttp3.OkHttpClient;
-import okhttp3.Request;
-import okhttp3.RequestBody;
-import okhttp3.Response;
+  private static final String TAG = "MainActivity";
+  private QRCodeReaderView qrCodeReaderView;
+  private EditText qrCodeEditText;
+  private FloatingActionButton fab;
+  private ImageButton clearTextButton;
 
-public class MainActivity extends AppCompatActivity implements QRCodeReaderView.OnQRCodeReadListener{
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
 
-    private QRCodeReaderView qrCodeReaderView;
-    private TextView qrcode;
-    private FloatingActionButton fab;
-    private String code;
-    private ImageButton cancelButton;
-    private String REGISTRATION_URL = "";
-    private final OkHttpClient client = new OkHttpClient();
-    private ProgressDialog dialog;
+    qrCodeReaderView = findViewById(R.id.qrdecoderview);
+    qrCodeEditText = findViewById(R.id.qr_code);
+    fab = findViewById(R.id.fab);
+    clearTextButton = findViewById(R.id.btn_clear_text);
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+    clearTextButton.setOnClickListener(this);
+    fab.setOnClickListener(this);
 
-        dialog = new ProgressDialog(this);
+    qrCodeReaderView.setOnQRCodeReadListener(this);
+    // Use this function to enable/disable decoding
+    qrCodeReaderView.setQRDecodingEnabled(true);
 
-        qrCodeReaderView = findViewById(R.id.qrdecoderview);
-        qrcode = findViewById(R.id.qr_code);
-        fab = findViewById(R.id.fab);
-        cancelButton = findViewById(R.id.cancel_button);
-        cancelButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                qrcode.setText("");
-                code = "";
-                fab.hide();
-            }
-        });
-        fab.hide();
+    // Use this function to change the autofocus interval (default is 5 secs)
+    qrCodeReaderView.setAutofocusInterval(2000L);
 
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                fab.hide();
-                //register him
-                if (code != null && !code.equals("") && code.length() > 4)
-                    try {
-                        //show a loading dialog
-                        dialog.setTitle("Registering "+code);
-                        dialog.setCancelable(true);
-                        dialog.show();
-                        registerThisGuy(code);
-                    } catch (Exception e) {
-                        e.printStackTrace();
-                    }
+    // Use this function to enable/disable Torch
+    qrCodeReaderView.setTorchEnabled(false);
 
-                Toast.makeText(MainActivity.this, code, Toast.LENGTH_LONG).show();
-                code = "";
-                qrcode.setText("");
-            }
-        });
+    // Use this function to set back camera preview
+    qrCodeReaderView.setBackCamera();
+  }
 
-        qrCodeReaderView.setOnQRCodeReadListener(this);
-        // Use this function to enable/disable decoding
-        qrCodeReaderView.setQRDecodingEnabled(true);
 
-        // Use this function to change the autofocus interval (default is 5 secs)
-        qrCodeReaderView.setAutofocusInterval(2000L);
+  @Override
+  public void onClick(View view) {
+    switch (view.getId()) {
+      case R.id.btn_clear_text:
+        qrCodeEditText.setText("");
+        break;
+      case R.id.fab:
+        validate();
 
-        // Use this function to enable/disable Torch
-        qrCodeReaderView.setTorchEnabled(false);
-
-        // Use this function to set back camera preview
-        qrCodeReaderView.setBackCamera();
+        break;
     }
+  }
 
-    @Override
-    public void onQRCodeRead(String text, PointF[] points) {
-        Toast.makeText(this, text, Toast.LENGTH_LONG).show();
-        code = text;
-        qrcode.setText(text);
-        if (!fab.isShown())
-            fab.show();
+  private void validate() {
+    if (!TextUtils.isEmpty(qrCodeEditText.getText())) {
+      String code = qrCodeEditText.getText().toString().toUpperCase();
+      if (code.length() < 6) {
+        Toast.makeText(this, "Invalid Code!", Toast.LENGTH_SHORT).show();
+      } else {
+        register(code);
+      }
     }
+  }
 
-    public void registerThisGuy(String Code) throws Exception{
-        RequestBody formBody = new FormBody.Builder()
-                .add("code", code)
-                .build();
-        Request request = new Request.Builder()
-                .url(REGISTRATION_URL)
-                .post(formBody)
-                .build();
-
-        try (Response response = client.newCall(request).execute()) {
-            dialog.dismiss();
-            if (!response.isSuccessful()) throw new IOException("Unexpected code " + response);
-
-            //got the response
-            Log.i("QRCODE", response.body().string());
-        }
+  @Override
+  public void onQRCodeRead(String text, PointF[] points) {
+    qrCodeEditText.setText(text);
+    if (!fab.isShown()) {
+      fab.show();
     }
+  }
 
-    @Override
-    public void onBackPressed() {
-        if (dialog.isShowing())
-            dialog.dismiss();
-        else
-            super.onBackPressed();
+  public void register(String code) {
+    qrCodeReaderView.stopCamera();
+
+    Call<VerificationResponse> call = RemoteApi.createService(QRCodeVerificationService.class)
+        .verifyCode(code);
+    call.enqueue(new Callback<VerificationResponse>() {
+      @Override
+      public void onResponse(Call<VerificationResponse> call,
+          Response<VerificationResponse> response) {
+
+        Log.d(TAG, "onResponse: " + response.body().toString());
+
+        qrCodeReaderView.startCamera();
+      }
+
+      @Override
+      public void onFailure(Call<VerificationResponse> call, Throwable t) {
+        Log.e(TAG, "onFailure: " + t.getLocalizedMessage(), t);
+
+        qrCodeReaderView.startCamera();
+      }
+    });
+
+
+  }
+
+  @Override
+  public void onBackPressed() {
+    finishAffinity();
+  }
+
+  @Override
+  protected void onResume() {
+    super.onResume();
+    if (qrCodeReaderView != null) {
+      qrCodeReaderView.startCamera();
     }
+  }
+
+  @Override
+  protected void onPause() {
+    super.onPause();
+    if (qrCodeReaderView != null) {
+      qrCodeReaderView.stopCamera();
+    }
+  }
+
+  @Override
+  public void onDismiss(DialogInterface dialog) {
+    if (qrCodeReaderView != null) {
+      qrCodeReaderView.startCamera();
+    }
+  }
+
 }
